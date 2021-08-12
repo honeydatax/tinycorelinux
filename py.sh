6d905cd53e96f3bfb925395adf1017460c0ba8ef
lisbon <16432726+honeydatax@users.noreply.github.com>
2021-08-12 19:58:14 +0100

    Py as server

---
+++ py.sh
@@ -0,0 +1,22 @@
+printf "\e[37;45m"
+tce-load -i python3.4.tcz
+tce-load -i python-dev.tcz
+tce-load -i python-RPi.GPIO.tcz
+tce-load -i python-cracklib.tcz
+tce-load -i python-dev.tcz
+tce-load -i python-libproxy.tcz
+tce-load -i python-ntplib.tcz
+tce-load -i python.tcz
+tce-load -i python3.4-dev.tcz
+tce-load -i python3.4.tcz
+tce-load -i glib2-python.tcz
+tce-load -i hostapd.tcz
+tce-load -i libnl.tcz
+tce-load -i libssh2-dev.tcz
+tce-load -i libssh2.tcz
+tce-load -i lighttpd-dev.tcz
+tce-load -i lighttpd.tcz
+tce-load -i samba4-lib.tcz
+tce-load -i samba4.tcz
+
+
---
+++ server.sh
@@ -0,0 +1,5 @@
+printf "\e[37;45m"
+ifconfig 
+python -m SimpleHTTPServer 8080
+
+
