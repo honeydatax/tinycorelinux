733706be053e7276f9b5e8b2a046413191cd2cb4
lisbon <16432726+honeydatax@users.noreply.github.com>
2021-08-15 10:51:37 +0100

    Add files via upload

---
+++ Loadmono.sh
@@ -0,0 +1,4 @@
+wget "http://repo.tinycorelinux.net/7.x/armv6/tcz/mono-dev.tcz"
+wget "http://repo.tinycorelinux.net/7.x/armv6/tcz/mono-doc.tcz"
+wget "http://repo.tinycorelinux.net/7.x/armv6/tcz/mono-locale.tcz"
+wget "http://repo.tinycorelinux.net/7.x/armv6/tcz/mono.tcz"
---
+++ go.sh
@@ -0,0 +1,4 @@
+printf "\e[37;45m"
+tce-load -i go.tcz
+
+
