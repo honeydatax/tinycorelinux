594d59c61be3eb40b33014c4ee4b59740b738bc9
lisbon <16432726+honeydatax@users.noreply.github.com>
2021-08-15 06:05:23 +0100

    Add files via upload

---
+++ Loadsql.sh
@@ -0,0 +1,9 @@
+printf "\e[37;45m"
+wget "http://repo.tinycorelinux.net/7.x/armv6/tcz/sqlite3-bin.tcz"
+wget "http://repo.tinycorelinux.net/7.x/armv6/tcz/sqlite3-dev.tcz"
+wget "http://repo.tinycorelinux.net/7.x/armv6/tcz/sqlite3-doc.tcz"
+wget "http://repo.tinycorelinux.net/7.x/armv6/tcz/sqlite3.tcz"
+wget "http://repo.tinycorelinux.net/7.x/armv6/tcz/libedit.tcz"
+wget "http://repo.tinycorelinux.net/7.x/armv6/tcz/ncurses.tcz"
+
+
