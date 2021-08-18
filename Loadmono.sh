fd0026e3ebc2608e882be3e945e66b0f1bd56b9c
lisbon <16432726+honeydatax@users.noreply.github.com>
2021-08-18 08:14:47 +0100

    Delete Loadmono.sh

--- Loadmono.sh
+++
@@ -1,4 +0,0 @@
-wget "http://repo.tinycorelinux.net/7.x/armv6/tcz/mono-dev.tcz"
-wget "http://repo.tinycorelinux.net/7.x/armv6/tcz/mono-doc.tcz"
-wget "http://repo.tinycorelinux.net/7.x/armv6/tcz/mono-locale.tcz"
-wget "http://repo.tinycorelinux.net/7.x/armv6/tcz/mono.tcz"
