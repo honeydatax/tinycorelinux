4f99efc3b5a20886b896593928e886baef179c56
lisbon <16432726+honeydatax@users.noreply.github.com>
2021-08-18 08:12:01 +0100

    Delete sql.sh

--- sql.sh
+++
@@ -1,9 +0,0 @@
-printf "\e[37;45m"
-tce-load -i sqlite3-bin.tcz
-tce-load -i sqlite3-dev.tcz
-tce-load -i sqlite3-doc.tcz
-tce-load -i sqlite3.tcz
-tce-load -i libedit.tcz
-tce-load -i ncurses.tcz
-
-
